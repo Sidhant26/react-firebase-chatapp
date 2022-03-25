5a4c2bb71c1252da147393c07d3d8e09396b3fd4
Sidhant <sidhant26sood@gmail.com>
2022-03-25 19:09:03 +0530

    integrated Firebase w/ React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'reeact'
+import './App.css'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+import {useAuthState} from 'react-firebase-hooks/auth'
+
+const firebaseConfig={
+  apiKey: "AIzaSyDmFgSbTGcJ2uPUJRE35pEO0suUSGSolfE",
+  authDomain: "react-firebase-chatapp-50ffa.firebaseapp.com",
+  projectId: "react-firebase-chatapp-50ffa",
+  storageBucket: "react-firebase-chatapp-50ffa.appspot.com",
+  messagingSenderId: "1041695373842",
+  appId: "1:1041695373842:web:fbfaef0c3c44d6299276cb",
+  measurementId: "G-JPFC1BMJFP"
+}
+
+firebase.initializeApp({firebaseConfig})
+
+const auth=firebase.auth(), firestore=firebase.firestore()
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
