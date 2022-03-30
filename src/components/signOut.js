8a4aff6c52354293ee63aafc307048d9c74e3683
Sidhant <sidhant26sood@gmail.com>
2022-03-30 19:20:22 +0530

    added CSS

--- src/components/signOut.js
+++ src/components/signOut.js
@@ -6,11 +6,9 @@ import {auth} from "../firebase"
 function SignOut() {
   return auth.currentUser && (
     <div style={{
-      display: 'flex', justifyContent: 'center', position: 'fixed', width: '100%', backgroundColor: '#FAFAFA', top: 0, 
-      borderBottom: 'solid 1px lightgray', zIndex: '10'
-  }}>
+      display: 'flex', justifyContent: 'flex-end', position: 'fixed', width: '100%', top: 0}}>
         <Button style={{ 
-          padding: '20px', fontSize: '20px', fontWeight: '250', color: 'gray'}}onClick={()=>auth.signOut()}>Sign out</Button>
+          padding: '10px', fontSize: '20px', fontWeight: '250', color: 'gray'}}onClick={()=>auth.signOut()}>Sign out</Button>
     </div>
   )
 }
